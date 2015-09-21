bbbe43a4c75abc7d80299b459cbad0af7bcfbe55
krishnakumar4a4 <krishna.thokala2010@gmail.com>
2015-09-22 00:54:41 +0530

    Now it holds only the client functionality
    But still unclear why the broadcast messages from server doesn't show up on client side, may have to check with implementation cast.

--- chat_client.erl
+++ chat_client.erl
@@ -2,16 +2,31 @@
 -behavior(gen_server).
 
 -export([init/1,handle_call/3,handle_cast/2]).
--export([start_client/2]).
+-export([start_client/1,new_client/1]).
 
--record(state,{client_state,server_pid}).
+-record(state,{client_state,server_name}).
 
-start_client(ClientName,ServPid)->
-    gen_server:start_link({local,ClientName},?MODULE,{ServPid,client_initialised},[{timeout,100}]).
+new_client(ClientName) ->
+	ClientDetails = case start_client(ClientName) of
+		{ok, ClientPid} ->
+		    {client_started,ClientName,ClientPid};
+		_ ->
+		    not_started
+	    end,
+    case gen_server:call({global,chat_server}, {link_client,ClientDetails}, infinity) of
+	client_linked ->
+	    gen_server:call(ClientName,{go_live,ClientName},infinity),
+		io:format("client ~p linked to chat_server",[ClientName]);
+	_ ->
+	    io:format("client ~p : not successfully linked",[ClientName])
+    end.
+	
+start_client(ClientName)->
+    gen_server:start_link({local,ClientName},?MODULE,{client_initialised},[{timeout,100}]).
     
-init({ServPid,INIT}) ->
+init({INIT}) ->
     process_flag(trap_exit,true),
-    State = #state{client_state = INIT,server_pid=ServPid},
+    State = #state{client_state = INIT,server_name=chat_server},
     io:format("server pid assigned to  client is ~p~n",[State]),
     {ok,State}.
 
