2789647e5489764a77f94f5be8d1efacf1dc66c7
krishnakumar4a4 <krishna.thokala2010@gmail.com>
2015-09-26 00:34:45 +0530

    Completely refactored code with gen_tcp
    This gives connect request to the server on a hardcoded ip and port. If connection is established client is linked to the server. 
    1 is appended during the ConnectionRequest and 2 is appended while sending msgs to get identified by server easily which is connection request and which is msg.
    After client linking a process is spawned to listen to incoming broadcasts continuously.
    Still had doubts with handle cast here.

--- chat_client.erl
+++ chat_client.erl
@@ -4,25 +4,46 @@
 -export([init/1,handle_call/3,handle_cast/2]).
 -export([start_client/1,new_client/1]).
 
--record(state,{client_state,server_name}).
+-record(state,{client_state,server_name,socket}).
 
 new_client(ClientName) ->
-	ClientDetails = case start_client(ClientName) of
+	start_client(ClientName).
+
+start_client(ClientName)->
+    ClientDetails = case gen_server:start_link({local,ClientName},?MODULE,{client_initialised},[{timeout,100}]) of 
 		{ok, ClientPid} ->
 		    {client_started,ClientName,ClientPid};
 		_ ->
 		    not_started
 	    end,
-    case gen_server:call({global,chat_server}, {link_client,ClientDetails}, infinity) of
-	client_linked ->
-	    gen_server:call(ClientName,{go_live,ClientName},infinity),
-		io:format("client ~p linked to chat_server",[ClientName]);
-	_ ->
-	    io:format("client ~p : not successfully linked",[ClientName])
-    end.
-	
-start_client(ClientName)->
-    gen_server:start_link({local,ClientName},?MODULE,{client_initialised},[{timeout,100}]).
+	case gen_tcp:connect({192,168,1,13},9000,[binary,{active,false}]) of
+		{ok,Socket} ->
+			ConnectionRequest = [1]++[atom_to_list(element(2,ClientDetails))],
+			case gen_tcp:send(Socket,ConnectionRequest) of 
+				ok ->
+					case gen_tcp:recv(Socket,0) of
+						{ok,<<"client_linked">>} ->
+							gen_server:call(ClientName,{go_live,ClientName,Socket},infinity),
+							io:format("client ~p linked to chat_server~n",[ClientName]),
+							spawn(fun() -> listen_broadcast(Socket,ClientName) end);
+						_ ->
+							io:format("client ~p : not successfully linked~n",[ClientName])
+					end;
+				Reason ->
+					io:format("The data was not sent,reason ~p~n",[Reason])
+				end;
+		{error,Reason} ->
+			io:format("connection request wasn't successful ~p~n",[Reason])
+	end.
+
+listen_broadcast(Socket,ClientName) ->
+	case gen_tcp:recv(Socket,0) of
+		{ok,INMSG} ->
+			gen_server:cast(ClientName,{broadcast,ClientName,INMSG});
+		Reason ->
+			io:format("Incoming broadcast failed,reason ~p ~p ~p~n",[Reason,Socket,ClientName])
+	end,
+	listen_broadcast(Socket,ClientName).
     
 init({INIT}) ->
     process_flag(trap_exit,true),
@@ -30,15 +51,23 @@ init({INIT}) ->
     io:format("server pid assigned to  client is ~p~n",[State]),
     {ok,State}.
 
-handle_call({go_live,ClientName},_From,_State) ->
-    keep_listening(ClientName).
+handle_call({go_live,ClientName,Socket},_From,State) ->
+	NewState = State#state{socket = Socket},
+    keep_listening(ClientName,Socket),
+	{reply,gone_live,NewState}.
 
-keep_listening(ClientName) ->
+keep_listening(ClientName,Socket) ->
     OUTMSG = io:get_line(atom_to_list(ClientName)++"> "),
-    gen_server:call({global,chat_server},{in_msg,OUTMSG},infinity),
-    keep_listening(ClientName).
+	OUTMSG1 = [2|[length(atom_to_list(ClientName))|[atom_to_list(ClientName) ++ ">" ++ OUTMSG]]],
+	io:format("out msg from client ~p~n",[OUTMSG1]),
+	case gen_tcp:send(Socket,OUTMSG1) of
+		ok ->
+			keep_listening(ClientName,Socket);
+		Reason ->
+			io:format("The data was not sent,reason ~p",[Reason])
+	end.
 
 handle_cast({broadcast,ClientName,INMSG},_State) ->
-    io:fwrite("~p> ~p",[ClientName,INMSG]),
-	io:format("~p> ~p",[ClientName,INMSG]).
+    io:fwrite("~p> ~p~n",[ClientName,INMSG]),
+	io:format("~p> ~p~n",[ClientName,INMSG]).
     
